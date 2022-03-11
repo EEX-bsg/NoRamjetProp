34fdbc3fd34c39c17e8bfea5ba7f49d67e88513c
EEX-bsg <exendra314@gmail.com>
2022-03-11 15:27:34 +0900

    fix NoRamjetPropController

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # NoRamjetProp
+
+[日本語版はこちら]()
+## Overview
+![png](https://i.gyazo.com/80fff9139f856812f1cf1847edde0067.png)  
+This is a Besiege mod that disable the ramjet propeller.
+## Requirement
+- Besiege(64bit)  
+※PC version for steam only, not compatible with Xbox, GamePass, etc.
+## Usage
+1. Subscribe in steam workshop.
+2. Turn it on in the mod menu on the Besiege title screen.
+## Description
+### How this mod disables ramjet prop.  
+The ramjet acts on the propeller because the vertical direction of the velocity sensing surface and the vector of the force on the propeller are misaligned.  
+※For more information on "velocity sensing surface", see the Ramjet description page below.  
+[JP-wiki](https://w.atwiki.jp/besiegejpwiki/pages/91.html)  
+  
+This mod makes the propeller force perpendicular to the velocity sensing surface.  
+This allows the propeller to work as it appears and no ramjet is generated.  
+
+## Author
+EEX-bsg  
+[twitter](https://twitter.com/EEX_bsg)
+
+## Licence
+[MIT]()
+
+### Last updated
+2022/3/11
--- src/NoRamjetPropScript/NoRamjetPropController.cs
+++ src/NoRamjetPropScript/NoRamjetPropController.cs
@@ -10,14 +10,13 @@ namespace NoRamjetPropNS
 
         private Rigidbody rigidBody;
         private Vector3 currentVelocity;
-        private Transform upTransform;
-        private Vector3 AxisDrag = new Vector3(0, 0.015f, 0);
+        private Transform upTransform;//速度検出面と言われるもの
+        private Vector3 AxisDrag = new Vector3(0, 0.015f, 0);//空力のかかる量
+        private float velocityCap = 30f;//この速度より小さいと、速度の３乗比例で空力かかる
+        private Vector3 liftNormalRot;//ペラの水平角度
         private Vector3 xyz;
         private float currentVelocitySqr;
-        private float velocityCap = 100f;
         private float sqrCap;
-        private Vector3 liftNormalRot;//ペラの水平角度
-        private int id;//ブロックID
         private PropellorController propController;
 
         public void Awake()
@@ -30,18 +29,8 @@ namespace NoRamjetPropNS
         public void Start()
         {
             rigidBody = base.GetComponent<Rigidbody>();//ブロックのrigidbody取得
-            propController = base.GetComponent<PropellorController>();
-            id = propController.BlockID;
-            switch(id)
-            {
-                case 26:
-                    liftNormalRot = new Vector3(0, 0, 23.06876f);
-                    break;
-                case 52:
-                case 55:
-                    liftNormalRot = new Vector3(0, 0, 22.845f);
-                    break;
-            }
+            propController = base.GetComponent<PropellorController>();//既存のプロペラコントローラーを取得
+            liftNormalRot = new Vector3(0, 0, propController.upTransform.localEulerAngles.z);//ペラの水平角度を持ってくる Fで反転するから固定値ではダメ
             propController.AxisDrag = new Vector3(0, 0, 0);//既存プロペラコントローラーの空気抵抗をゼロにする
         }
         public void FixedUpdate()
@@ -62,12 +51,12 @@ namespace NoRamjetPropNS
         {
             upTransform = propController.upTransform;
             currentVelocity = rigidBody.velocity;
-            Vector3 vector = upTransform.InverseTransformDirection(currentVelocity);
+            Vector3 vector = upTransform.InverseTransformDirection(currentVelocity);//ここで速度検出面から速度を取得してる
             xyz = Vector3.Scale(-vector, AxisDrag);
             currentVelocitySqr = Mathf.Min(currentVelocity.sqrMagnitude, sqrCap);
             Vector3 force = xyz * currentVelocitySqr;
             Quaternion qua = Quaternion.FromToRotation(Vector3.up, upTransform.up);
-            rigidBody.AddRelativeForce(Quaternion.Euler(liftNormalRot) * force);
+            rigidBody.AddRelativeForce(Quaternion.Euler(liftNormalRot) * force);//速度検出面垂直方向に空力を傾けてる(謎加速を消してる)
         }
     }
 }
